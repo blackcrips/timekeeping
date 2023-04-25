c29c67e7509b46d73f64954730fb9166d8b58201
Jimmy Consulta <jimmy.consulta@Valtes.intra>
2023-04-25 23:05:01 +0800

    Adding header design

--- header.php
+++ header.php
@@ -1,27 +1,39 @@
-<div class="container-header">
-    <div class="header-content">
-        <div class="profile-photo">
-            <img src="./images/empty-photo.jpg" alt="Empty">
-        </div>
-        <div class="container-leave">
-            <div class="remaining-vl">Remaining Leave: <span>2</span></div>
-            <div class="remaining-sl">Remaining Sick Leave: <span>4</span></div>
-        </div>
-        <div class="timekeeping-actions">
-            <div class="container-time-in">
-                <button class="time-in action" disabled>Time in</button>
-            </div>
-            <div class="container-first-break">
-                <button class="first-break-in action" disabled>First Break In</button>
-                <button class="first-end-break action" disabled>First End Break</button>
-            </div>
-            <div class="container-second-break">
-                <button class="second-break-in action" disabled>Second Break In</button>
-                <button class="second-end-break action" disabled>Second End Break</button>
-            </div>
-            <div class="container-timeout">
-                <button class="time-out action" disabled>Time out</button>
+<?php 
+date_default_timezone_set('Asia/Manila');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/header.css" class="rel">
+    <script src="./js/header.js" defer></script>
+    <title>Company Time Management</title>
+</head>
+<body>
+
+    <div class="cont__pageHeader">
+        <div class="ph_rightContent">
+            <div class="app_title">
+                <img id='logo' src="./images/comp-logo.png" alt="Logo">
             </div>
         </div>
+        <div class="ph_leftContent">
+            <div class="cont__dateAndTime">
+                <div class="contDate">
+                    <?php echo date('l F j, Y');?>
+                </div>
+                <div class="contTime">
+                    <?php echo date('h:i:s A');?>
+                </div>
+            </div>
+        </div>
+        <!-- <div class="ph_greet">
+            <div> Good afternoon </div>
+            <div class='nthGreet'> jimmy! </div>
+        </div> -->
     </div>
-</div>
+
--- homePage.php
+++ homePage.php
@@ -1,20 +1,10 @@
 <?php
-
+include 'header.php';   
 include_once('./includes/autoLoadClassesMain.inc.php');
 date_default_timezone_set('Asia/Manila');
 $controller = new Controller();
 $controller->redirectForeignUser();
-
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
 <body>
     <a href="leavePage.php">File Leave</a>
     <br>
@@ -29,7 +19,7 @@ $controller->redirectForeignUser();
 
 
     <form action="./includes/logout.inc.php" method="post">
-        <button name="logout">Logout</button>
+        <button class='btn btn-dark' name="logout">Logout</button>
     </form>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js" integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
